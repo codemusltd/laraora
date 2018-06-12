11e4b19b2e2ee044888f2e4707389ece4ce22259
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2018-06-12 19:12:22 +0100

    1. re-write code in react for easier manageability 2. first commit includes navbar and boutique components, sans css

---
+++ Boutique.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Boutique = (props) =>{
+    return(
+      <div id='two' className='w3-display-container'>
+      <div className='w3-display-container w3-text-white'>
+          <img src='https://static1.squarespace.com/static/53431644e4b0cf4885e907f7/t/593068b56a496379d8588858/1496344926799/unnamed-1.jpg' 
+              alt='beauty' style={{width:'100%', height:'850px'}}/>
+          <div className='w3-xxxlarge w3-display-middle w3-padding w3-text-black'><span className='w3-s-cap w3-text-black'>beauty boutique</span>
+              <div className='w3-row w3-text-white'>
+                  <div className='w3-third w3-center'>
+                      <h3 className='w3-dark-grey w3-s-cap w3-button w3-round-xxlarge w3-padding-large'>Salon</h3>
+                  </div>
+                  <div className='w3-third w3-center'>
+                      <h3 className='w3-dark-grey w3-s-cap w3-button w3-round-xxlarge w3-padding-large'>Spa</h3>
+                  </div>
+                  <div className='w3-third w3-center w3-margin-bottom'>
+                      <h3 className='w3-dark-grey w3-s-cap w3-button w3-round-xxlarge w3-padding-large'>Apparel</h3>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+    )
+  }
+  
+//   ReactDOM.render(<Boutique/>, document.getElementById('main'));
---
+++ Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Nav = (props) =>{
+  return(
+    <div id='one' classNameName='w3-padding'>
+        <div classNameName='w3-inblock'>
+            <img src='http://static1.squarespace.com/static/53431644e4b0cf4885e907f7/t/593064de9de4bb482f2cc61b/1524592987210/?format=1500w' 
+              alt='logo' id='logo' classNameName='w3-padding'/> 
+        </div>
+        <div className='w3-inblock w3-display-right w3-padding-right'>
+            <ul id='navList' classNameName=''>
+                <li classNameName='w3-button w3-bar-item w3-s-cap'>reviews</li>
+                <li classNameName='w3-button w3-bar-item w3-s-cap'>request appointments</li>
+                <li classNameName='w3-button w3-bar-item w3-s-cap'>salon</li>
+                <li classNameName='w3-button w3-bar-item w3-s-cap'>spa</li>
+                <li classNameName='w3-button w3-bar-item w3-s-cap'>bridal</li>
+                <li classNameName='w3-button w3-bar-item w3-s-cap'>apparel</li>
+                <li classNameName='w3-button w3-bar-item w3-s-cap'>our team</li>
+                <li classNameName='w3-button w3-bar-item w3-s-cap'>contact us</li>
+            </ul>
+        </div>
+   </div>
+  )
+}
+
+// ReactDOM.render(<Nav/>, document.getElementById('main'));
